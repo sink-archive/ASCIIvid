9ac0caa2c283d8c0a790c3c24fb12a70b3a0de74
cainy-a <cainy-a@users.noreply.github.com>
2021-01-18 19:29:52 +0000

    AsciiVid.NET ColourCell

--- lib/AsciiVid.NET/AsciiVid/AsciiImg/ColourCell.cs
+++ lib/AsciiVid.NET/AsciiVid/AsciiImg/ColourCell.cs
@@ -2,8 +2,19 @@
 {
 	public class ColourCell : Cell
 	{
+		/// <summary>
+		///     The red colour channel of the cell
+		/// </summary>
 		public byte RedChannel;
+
+		/// <summary>
+		///     The green colour channel of the cell
+		/// </summary>
 		public byte GreenChannel;
+
+		/// <summary>
+		///     The blue colour channel of the cell
+		/// </summary>
 		public byte BlueChannel;
 	}
 }
