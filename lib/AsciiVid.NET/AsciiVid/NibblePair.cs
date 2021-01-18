7b7b76eba347ae391fd0e65d6ccfc50792c0a9c5
cainy-a <cainy-a@users.noreply.github.com>
2021-01-18 20:02:50 +0000

    Move NibbleTools and other handy things to AsciiVid.Utilities

--- lib/AsciiVid.NET/AsciiVid/NibblePair.cs
+++ lib/AsciiVid.NET/AsciiVid/NibblePair.cs
@@ -51,32 +51,8 @@ namespace AsciiVid
 
 		public byte Value
 		{
-			get => RawBinary.GetLowNibble();
+			get => RawBinary.GetHighNibble();
 			set => RawBinary.SetLowNibble(value);
 		}
-	}
-
-	public static class NibbleTools
-	{
-		public static byte GetLowNibble(this  byte input) => (byte) ((input & 0xF0) >> 4);
-		public static byte GetHighNibble(this byte input) => (byte) (input & 0x0F);
-
-		public static void SetLowNibble(this byte input, byte newValue)
-		{
-			if (!newValue.IsValidNibble())
-				throw new ArgumentOutOfRangeException(nameof(newValue),
-				                                      "Too big for a nibble! Store a value between 0-15");
-			input = (byte) (input & (0xF0 + newValue));
-		}
-
-		public static void SeHighNibble(this byte input, byte newValue)
-		{
-			if (!newValue.IsValidNibble())
-				throw new ArgumentOutOfRangeException(nameof(newValue),
-				                                      "Too big for a nibble! Store a value between 0-15");
-			input = (byte) (input & (0x0F + (newValue << 4)));
-		}
-
-		public static bool IsValidNibble(this byte input) => input < 16;
 	}
 }
